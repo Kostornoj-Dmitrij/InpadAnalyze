8b2c2d7d338a64a561447304184c4909305615a6
Kostornoj-Dmitrij <D1.imosa@yandex.ru>
2025-04-25 17:15:02 +0300

    pages fix

--- bot/services/file_processing.py
+++ bot/services/file_processing.py
@@ -11,8 +11,8 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.platypus import Paragraph, SimpleDocTemplate
 
-project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+#project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 print(project_dir)
 font_path = os.path.join(project_dir, 'fonts/DejaVuSans.ttf')
